769325b08f91865b71729ecd003c49f6263a613f
AnnZanevskaya <ann.zanevskaya7@gmail.com>
2020-03-24 22:36:48 +0300

    feat: implement scroll behaviour

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ document.querySelector(".modal__confirm").onclick = function () {
   CONTACTMODAL.style.display = "none";
 }
 
+const MAINSECTIONSPOSITION = [];
+
 window.onclick = function (event) {
   if (event.target == CONTACTMODAL) {
     CONTACTMODAL.style.display = "none";
@@ -35,7 +37,10 @@ function fillFormTemplate() {
 }
 
 window.onload = function () {
+  initMainSectionsPosition();
   addNavClickHandler();
+
+  window.addEventListener('scroll', selectNavLinkByScrolling);
 }
 
 const addNavClickHandler = () => {
@@ -43,7 +48,7 @@ const addNavClickHandler = () => {
     if (e.target.classList.contains('navigation__link')) {
       let clickedNav = e.target;
       removeSelectedNav();
-      selectClickedNav(clickedNav);
+      selectNav(clickedNav);
     }
   });
 }
@@ -56,6 +61,69 @@ const removeSelectedNav = () => {
   });
 }
 
-const selectClickedNav = (clickedNav) => {
+const selectNav = (clickedNav) => {
   clickedNav.classList.add('navigation__link_active');
+}
+
+const selectNavLinkByScrolling = () => {
+  let nearUserSection = MAINSECTIONSPOSITION[0].key;
+
+  MAINSECTIONSPOSITION.forEach(sectionPosition => {
+    let userScroll = window.scrollY;
+
+    if (userScroll >= sectionPosition.value) {
+      nearUserSection = sectionPosition.key;
+    }
+  });
+
+  let currentSectedNav = document.querySelector('.navigation__link_active');
+  let currenSectedNavValue = getNavLinkValue(currentSectedNav);
+
+  if (currenSectedNavValue !== nearUserSection) {
+    removeSelectedNav();
+
+    let nav = getNavLinkByValue(nearUserSection);
+    selectNav(nav);
+  }
+}
+
+const initMainSectionsPosition = () => {
+  var navLinks = getNavLinks();
+
+  navLinks.forEach(navLink => {
+    let sectionOffset = document.querySelector(navLink).offsetTop;
+    console.log(sectionOffset - 50);
+    MAINSECTIONSPOSITION.push({
+      key: navLink,
+      value: sectionOffset - 50
+    });
+  });
+
+  MAINSECTIONSPOSITION.sort(function (a, b) {
+    return a[1] - b[1];
+  });
+}
+
+const getNavLinks = () => {
+  let navLinks = document.querySelectorAll('.navigation__link');
+
+  let links = [...navLinks].map(navLink => {
+    return getNavLinkValue(navLink);
+  });
+
+  return links;
+}
+
+const getNavLinkValue = (navLink) => {
+  return navLink.attributes.href.value;
+}
+
+const getNavLinkByValue = (value) => {
+  let navLinks = document.querySelectorAll(".navigation__link")
+
+  let navLink = [...navLinks].filter(el => {
+    return value === getNavLinkValue(el);
+  })[0];
+
+  return navLink;
 }
